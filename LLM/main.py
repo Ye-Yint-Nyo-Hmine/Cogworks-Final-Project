1566637f680a6791204f738f990f88ccc4817bf8
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2024-07-31 11:52:37 -0400

    added implementation and updated instructions

--- LLM/main.py
+++ LLM/main.py
@@ -5,6 +5,8 @@ from utils.cryptographer import encrypt, decrypt
 #from utils.sys_prompt import SYS_PROMPT as sys_prompt
 #from utils.sys_prompt import KEY
 from profiles.Profile import Profile
+from multiprocessing import Process
+
 
 def export_sys_prompt(prompt, PATH=r"LLM\utils\sys.txt"):
     with open(PATH, "w") as f:
@@ -71,52 +73,60 @@ def report_event(event):
                                     'Stranger detected near home', 'Baby crying detected', 'Fire detected')
             Events can also specify where it happened: such as 'near rear part of the house', 'near front door', 'near the garage'
             
+            !This function must be call in every loop, if there is no event happening, just give a 'False' argument for event
+            
     Returns:
         doesn't return anything when called,
         but does report the user of the event and function accordingly.
         
     eg:
     
-    # Note: Remember that this function can be called in your own files, it does not need to be called inside this main function
+    # Note: Remember that this function can be called in your own files, it does not need to be called inside this main function, except multi-process
     
-    if baby_monitor.baby_detected() == True:
-        report_event("Baby crying detected near front door")
-    if fire_detection == True:
-        report_event("Fire has been detected")
-        
-    You don't need to add anything to the function, but just need to call it
+    First you need to create a function, with a loop inside of it:
     
+    def foo():
+       while True:
+            detection = get_detection()  #!! This function should return a string if detected, else return False
+            report_event(detection)
+    
+    
+    Then, inside of this file at the end of the function, I gave instructions to multi-process it
     """
-    
-    pass
-    
+    if event != False:
+        result = chain.invoke({"time": datetime.today(), 
+                                "sys_prompt": sys_prompt,
+                                "user_profile": profile.try_load_profile(),
+                                "context": context, 
+                                "question": event})
+        
+        report_to(result)
+        print(f"ByteGPT: {result}")
+        context += f"\nSystem: {event}\nMe: {result}"
+    else:
+        pass
     
 
 
 def tokenize_report(prompt):
     if 'eventreport' in prompt:
         try:
-            
+            report = prompt.split('eventreport=')[1]
+            return report
         except:
             print("Error occurred while tokenizing report")
 
-def checkReport(msg):
-    tokenized_report = tokenize_report()
+def report_to(prompt):
+    """
+    Check if a report has been notified by ByteGPT, then additional actions base upon it (such as sms messages for report and so on)
+    
+    Note: This function is not completed yet, until a GUI is created
+    """
+    tokenized_report = tokenize_report(prompt)
+    
 
 
 def main():
-    print("Started")
-    user_name = input("Enter name: ") 
-    custom_instr = True if (input("Do you want to include/update specific information about yourself and home? (Y/n): ")).lower() == "y" else False
-    if custom_instr:
-        user_instr = input("Enter user info/instructions: ")
-        response_instr = input("Enter home info/instructions: ")
-        profile = initialize_profile(user_name, user_instr, response_instr)
-    else:
-        profile = initialize_profile(user_name)
-    
-    context = profile.try_load_history()
-    
     
     while True:
         user_input = input("You: ")
@@ -141,4 +151,29 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    print("Started")
+    user_name = input("Enter name: ") 
+    custom_instr = True if (input("Do you want to include/update specific information about yourself and home? (Y/n): ")).lower() == "y" else False
+    if custom_instr:
+        user_instr = input("Enter user info/instructions: ")
+        response_instr = input("Enter home info/instructions: ")
+        profile = initialize_profile(user_name, user_instr, response_instr)
+    else:
+        profile = initialize_profile(user_name)
+    
+    context = profile.try_load_history()
+    
+    
+    process1 = Process(target = main)
+    process1.start()
+    
+    
+    """
+    add all the other implementations
+    eg:
+    process2 = Process(target = foo) # note functions need to be in a loop checking after every few times
+    process2.start()
+    
+    
+    """
+    

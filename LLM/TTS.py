a18fd5ee8abd3e642fcb354194ba7ed3cdf37adc
Ye-Yint-Nyo-Hmine <yeyintnyohmine143@gmail.com>
2024-07-31 11:18:20 -0400

    added function for implementation

---
+++ LLM/TTS.py
@@ -0,0 +1,4 @@
+
+
+def speak():
+    pass
--- LLM/main.py
+++ LLM/main.py
@@ -61,6 +61,49 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 
+def report_event(event):
+    """
+    This is the function which you can deploy to integrate and implement other functions
+    
+    Args:
+    event: str
+            Event can be anything;  (ie: 'Unrecognized face detected', 'Known face detected, Matched: User name, Accuracy: X%',
+                                    'Stranger detected near home', 'Baby crying detected', 'Fire detected')
+            Events can also specify where it happened: such as 'near rear part of the house', 'near front door', 'near the garage'
+            
+    Returns:
+        doesn't return anything when called,
+        but does report the user of the event and function accordingly.
+        
+    eg:
+    
+    # Note: Remember that this function can be called in your own files, it does not need to be called inside this main function
+    
+    if baby_monitor.baby_detected() == True:
+        report_event("Baby crying detected near front door")
+    if fire_detection == True:
+        report_event("Fire has been detected")
+        
+    You don't need to add anything to the function, but just need to call it
+    
+    """
+    
+    pass
+    
+    
+
+
+def tokenize_report(prompt):
+    if 'eventreport' in prompt:
+        try:
+            
+        except:
+            print("Error occurred while tokenizing report")
+
+def checkReport(msg):
+    tokenized_report = tokenize_report()
+
+
 def main():
     print("Started")
     user_name = input("Enter name: ") 
